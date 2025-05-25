47c58c71ddcb9fdda517dc19bf63911d18b5c0f1
Askar Kasimov <kasimovaskar123@gmail.com>
2025-05-25 14:54:49 +0300

    i18n init

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import Authorization from './pages/Authorization';
 import Cabinet from './pages/Cabinet';
 import Products from './pages/Products';
 import Password from './pages/Password';
+import './i18n.ts'; // язык
 
 const router = createBrowserRouter([
   {

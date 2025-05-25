62af50aad218154f4d49902db0ef50e8efe6f82b
Askar Kasimov <kasimovaskar123@gmail.com>
2025-05-25 21:27:56 +0300

    header i18n + body in full height

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import styles from './App.module.scss';
 
 const App = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <div className={styles.container}>
+        <Outlet />
+      </div>
       <Footer />
     </>
   );
